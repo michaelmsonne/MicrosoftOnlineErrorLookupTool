0ade64c5c995f51b7de64b2ad78b3bef2f72912c
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2024-05-01 22:20:05 +0200

    Add

---
+++ MicrosoftOnlineErrorLookupTool/MicrosoftOnlineErrorLookupTool/MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace MicrosoftOnlineErrorLookupTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxErrorCodeInput = new System.Windows.Forms.TextBox();
+            this.buttonGetDetailsFromErrorCode = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStripMainForm = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxMessageDetails = new System.Windows.Forms.TextBox();
+            this.textBoxRemediationDetails = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStripMainForm.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxErrorCodeInput
+            // 
+            this.textBoxErrorCodeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxErrorCodeInput.Location = new System.Drawing.Point(72, 19);
+            this.textBoxErrorCodeInput.Name = "textBoxErrorCodeInput";
+            this.textBoxErrorCodeInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxErrorCodeInput.TabIndex = 0;
+            this.textBoxErrorCodeInput.Text = "50049";
+            // 
+            // buttonGetDetailsFromErrorCode
+            // 
+            this.buttonGetDetailsFromErrorCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGetDetailsFromErrorCode.Location = new System.Drawing.Point(178, 17);
+            this.buttonGetDetailsFromErrorCode.Name = "buttonGetDetailsFromErrorCode";
+            this.buttonGetDetailsFromErrorCode.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetDetailsFromErrorCode.TabIndex = 1;
+            this.buttonGetDetailsFromErrorCode.Text = "Submit";
+            this.buttonGetDetailsFromErrorCode.UseVisualStyleBackColor = true;
+            this.buttonGetDetailsFromErrorCode.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxErrorCodeInput);
+            this.groupBox1.Controls.Add(this.buttonGetDetailsFromErrorCode);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 49);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Microsoft Online error code lookup";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Error code:";
+            // 
+            // menuStripMainForm
+            // 
+            this.menuStripMainForm.BackColor = System.Drawing.Color.White;
+            this.menuStripMainForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStripMainForm.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMainForm.Name = "menuStripMainForm";
+            this.menuStripMainForm.Size = new System.Drawing.Size(646, 24);
+            this.menuStripMainForm.TabIndex = 4;
+            this.menuStripMainForm.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // textBoxMessageDetails
+            // 
+            this.textBoxMessageDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMessageDetails.Location = new System.Drawing.Point(89, 19);
+            this.textBoxMessageDetails.Multiline = true;
+            this.textBoxMessageDetails.Name = "textBoxMessageDetails";
+            this.textBoxMessageDetails.Size = new System.Drawing.Size(529, 78);
+            this.textBoxMessageDetails.TabIndex = 6;
+            // 
+            // textBoxRemediationDetails
+            // 
+            this.textBoxRemediationDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRemediationDetails.Location = new System.Drawing.Point(89, 103);
+            this.textBoxRemediationDetails.Multiline = true;
+            this.textBoxRemediationDetails.Name = "textBoxRemediationDetails";
+            this.textBoxRemediationDetails.Size = new System.Drawing.Size(529, 123);
+            this.textBoxRemediationDetails.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBoxMessageDetails);
+            this.groupBox2.Controls.Add(this.textBoxRemediationDetails);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 84);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(624, 235);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Error details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Message:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Remediation:";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(646, 329);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStripMainForm);
+            this.MainMenuStrip = this.menuStripMainForm;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Microsoft Online Error lookup";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStripMainForm.ResumeLayout(false);
+            this.menuStripMainForm.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxErrorCodeInput;
+        private System.Windows.Forms.Button buttonGetDetailsFromErrorCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStripMainForm;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxMessageDetails;
+        private System.Windows.Forms.TextBox textBoxRemediationDetails;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ MicrosoftOnlineErrorLookupTool/MicrosoftOnlineErrorLookupTool/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MicrosoftOnlineErrorLookupTool
+{
+    public partial class MainForm : Form
+    {
+        private const string ErrorUrl = "https://login.microsoftonline.com/error?code=";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string errorCode = textBoxErrorCodeInput.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(errorCode))
+            {
+                MessageBox.Show(@"Please enter an error code to lookup", @"Error - missing error code", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string errorPageUrl = ErrorUrl + WebUtility.UrlEncode(errorCode);
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string htmlCode = client.DownloadString(errorPageUrl);
+                    string[] errorDetails = ParseErrorDetails(htmlCode);
+
+                    // Set text boxes with returned values
+                    //textBoxErrorCodeDetails.Text = errorDetails[0];
+                    textBoxMessageDetails.Text = errorDetails[1];
+                    textBoxRemediationDetails.Text = errorDetails[2];
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show($@"Error: {ex.Message}", @"Web Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string[] ParseErrorDetails(string html)
+        {
+            string errorCode = "", message = "", remediation = "";
+
+            // Define regular expressions to match the error code, message, and remediation
+            Regex errorCodeRegex = new Regex(@"<td>Error Code<\/td>\s*<td>(.*?)<\/td>");
+            Regex messageRegex = new Regex(@"<td>Message<\/td>\s*<td>(.*?)<\/td>");
+            Regex remediationRegex = new Regex(@"<td>Remediation<\/td>\s*<td>(.*?)<\/td>");
+
+            // Match the regular expressions in the HTML content
+            Match errorCodeMatch = errorCodeRegex.Match(html);
+            if (errorCodeMatch.Success)
+            {
+                errorCode = errorCodeMatch.Groups[1].Value.Trim();
+            }
+
+            Match messageMatch = messageRegex.Match(html);
+            if (messageMatch.Success)
+            {
+                message = messageMatch.Groups[1].Value.Trim();
+            }
+
+            Match remediationMatch = remediationRegex.Match(html);
+            if (remediationMatch.Success)
+            {
+                remediation = remediationMatch.Groups[1].Value.Trim();
+            }
+
+            return new string[] { errorCode, message, remediation };
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
